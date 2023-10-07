e87211214fcc3ede584e9d28e0a5205a90c56524
MarlonGarc <marloncastillo424@gmail.com>
2023-10-06 21:21:33 -0600

    Ya solo falta agregar los datos de impresion, la opcion donde diga mostrar el nombre de los Integrantes del Grupo y los Usuarios con contraseñas

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 #include <conio.h>
 
-//QUE HOOONNNNDAAAAAAA ESTE COMENTARIO ES UNA PRUEA DE QUE PUDE SUBIR EL ARCHIVO
-
 using namespace std;
 int banco;
 string nombreBanco;
@@ -31,6 +29,51 @@ struct alumno
 	string profesion;
 	int graduacion;
 } alum;
+
+struct notas
+{
+    string carne;
+    string cod_curso;
+    string cod_cat;
+    int punt_par1;
+    int punt_par2;
+    int tareas;
+    int exam_cort;
+    int proyectos;
+    int zona;
+    int exa_fin;
+    int exa_retra; //solo si este perdio 
+} notas;
+      
+struct pagos 
+{              
+    int cod_pago;
+    string nom_pago;
+    string cod_alum;
+    string facultad;
+    int semestre;
+    float cuota_pago;
+}pag;
+              
+struct catedraticos
+{  
+	int cod_cat;
+    string nombre_cat;
+    string apellido_cat;
+    string direc_cat;
+    string profes_cat;
+    string tel_cat;
+    string correo_cat;
+} cat;
+	   
+struct cursos
+{                
+    int cod_curso;
+    string nom_curso;
+    string prerre; //PRERREQUISITOS
+    string creditos;
+    string horario;
+} cur;
 
 void ingresarDatos();
 void menuProceso();
@@ -60,7 +103,7 @@ void ingresarDatosBoletaInscripcion(){
 	ingresarDatos();
 };
 void ingresarDatosAlumnos(){
-    cout << "\nPor Favor Ingrese los Datos del Alumno";
+    cout << "\n\tPor Favor Ingrese los Datos del Alumno";
     cin.ignore();
     cout << "\nIngrese su Carnet: ";
 	getline(cin,alum.carnet);
@@ -89,16 +132,84 @@ void ingresarDatosAlumnos(){
 	ingresarDatos(); 
 };
 void ingresarDatosCursos(){
-    cout << "hola ingresa datos";
+    cout << "\n\tPor Favor Ingrese los Datos de los Cursos\n";
+    cin.ignore();
+    cout <<"\nIngresa El Codigo del Curso: ";
+    cin >> cur.cod_curso;
+    cout <<"Ingresa Nombre del Curso: ";
+    getline(cin,cur.nom_curso);
+    cin.ignore();
+    cout << "Ingrese los Prerrequisitos: ";
+    getline(cin,cur.prerre);
+    cout << "Ingrese los Creditos: ";
+    getline(cin,cur.creditos);
+    cout << "Ingrese el Horario: ";
+    getline(cin,cur.horario);
+    cout << "!Ingreso de Datos Completado con Exito!\n\n";
+    ingresarDatos();
 };
 void ingresarDatosCatedraticos(){
-    cout << "hola ingresa datos";
+    cout << "\n\tPor Favor Ingrese los Datos de los Catedraticos\n";
+    cin.ignore();
+    cout << "\nIngrese el Codigo del Catedratico: ";
+    cin>> cat.cod_cat;
+    cout << "Ingrese el Nombre del Catedratico: ";
+    getline(cin,cat.nombre_cat);
+    cin.ignore();
+    cout << "Ingrese el Apellido del Catedratico: ";
+    getline(cin,cat.apellido_cat);
+    cin.ignore();
+    cout << "Ingrese la Direccion: ";
+    getline(cin,cat.direc_cat);
+    cin.ignore();
+    cout << "Ingrese la Profesion: ";
+    getline(cin,cat.profes_cat);
+    cin.ignore();
+    cout << "Ingrese el Telefono: ";
+    getline(cin,cat.tel_cat);
+    cin.ignore();
+    cout << "Ingrese el Correo Electronico: ";
+    getline(cin,cat.correo_cat);
+    cin.ignore();
+    cout << "!Ingreso de Datos Completado con Exito!\n\n";
+    ingresarDatos();
 };
 void ingresarDatosNotas(){
-    cout << "hola ingresa datos";
+    cout << "\n\tPor Favor Ingrese los Datos de las Notas\n";
+    cin.ignore();
+    cout << "\nIngrese el Numero de Carne del Alumno: ";
+	getline(cin,notas.carne);
+	cout << "Ingrese el Codigo del Curso: ";
+	getline(cin,notas.cod_curso);
+	cout << "Ingrese el Codigo del Catedratico: ";
+	getline(cin,notas.cod_cat);
+	cout << "Ingrese la Nota del Primer Parcial: ";
+	cin >> notas.punt_par1;
+	cout << "Ingrese la Nota del Segundo Parcial: ";
+	cin >> notas.punt_par2;
+	cout << "Ingrese la Zona (Tareas, Examenes Cortos y Proyectos) del Alumno: ";
+	cin >> notas.zona;
+    cout << "!Ingreso de Datos Completado con Exito!\n\n";
+    ingresarDatos();
 };
 void ingresarPagos(){
-    cout << "hola ingresa datos";
+    cout << "\n\tPor Favor Ingrese los Datos de los Pagos\n";
+    cin.ignore();
+    cout << "\nIngrese el Codigo de Pago: ";
+    cin >> pag.cod_pago;
+	cin.ignore();
+    cout << "Ingrese el Nombre de Pago: ";
+    getline(cin,pag.nom_pago);
+    cout << "Ingrese el Codigo del Alumno: ";
+    getline(cin,pag.cod_alum);
+    cout << "Ingrese la Facultad: ";
+    getline(cin,pag.facultad);
+    cout << "Ingrese el Semestre: ";
+    cin >> pag.semestre;
+    cout << "Ingrese la Cuota de Pago: ";
+    cin >> pag.cuota_pago;
+    cout << "!Ingreso de Datos Completado con Exito!\n\n";
+    ingresarDatos();
 };	
 
 // Funciones para la impresión de datos
@@ -195,9 +306,79 @@ void ingresarDatos(){
 };
 void imprimirDatos(){
 	
+	cout << "\n\tElija Que Datos Desea Imprimir\n";
+	cout << "\n1.Impresion de Boleta para Pago de Inscripcion\n";
+	cout << "2.Inpresion de Talonario de Pagos\n";
+	cout << "3.Impresion de Pensum de Estudios\n";
+	cout << "4.Impresion de Cursos por Semestre\n";
+	cout << "5.Impresion de Notas por Alumno\n";
+	cout << "6.Impresion por semestre de alumnos inscritos en el 2023\n";
+	cout << "7.Impresion por alumno de Cursos Aprobados\n";
+	cout << "8.Impresion por Alumno de Cursos Reprobados\n";
+	cout << "9.Impresion de Pagos Realizados y Suma de Deuda\n";
+	cout << "10.Impresion de los Alumnos con los Mejores 3 Promedios por Semestre\n";
+	cout << "11.Regresar al Menu Anterior\n";
+	cout << "\nElija una Opcion: ";
+	
+	int imprimir = 0;
+	cin >> imprimir;
+
+	switch(imprimir){
+		case 1:
+			cout << "\n\t¡Sus Datos se an Impreso con Exito!";
+			imprimirBoletaPago();
+			imprimirDatos();
+			break;
+		case 2:
+			cout << "\n\t¡Sus Datos se an Impreso con Exito!";
+			
+			imprimirDatos();
+			break;
+		case 3:
+			cout << "\n\t¡Sus Datos se an Impreso con Exito!";
+			
+			imprimirDatos();
+			break;
+		case 4:
+			cout << "\n\t¡Sus Datos se an Impreso con Exito!";
+			
+			imprimirDatos();
+			break;
+		case 5:
+			cout << "\n\t¡Sus Datos se an Impreso con Exito!";
+			
+			imprimirDatos();
+			break;
+		case 6:
+			cout << "\n\t¡Sus Datos se an Impreso con Exito!";
+			
+			imprimirDatos();
+			break;
+		case 7:
+			cout << "\n\t¡Sus Datos se an Impreso con Exito!";
+			
+			imprimirDatos();
+			break;
+		case 8:
+			cout << "\n\t¡Sus Datos se an Impreso con Exito!";
+			
+			imprimirDatos();
+			break;
+		case 9:
+			cout << "\n\t¡Sus Datos se an Impreso con Exito!";
+			
+			imprimirDatos();
+			break;
+		case 10:
+			cout << "\n\t¡Sus Datos se an Impreso con Exito!";
+			
+			imprimirDatos();
+			break;
+		case 11:
+			menuProceso();
+	};
 };
 void regresarMenu(){
-	
 };
 
 void menuProceso(){
@@ -219,7 +400,6 @@ void menuProceso(){
 			imprimirDatos();
 			break;
 		case 3:
-			
 			system("cls");
 			main();
 			break;
@@ -244,7 +424,7 @@ string departementosMenu(){
 	}
 
 	int opDepartamento = 0;
-	cout << "\nElija un Departamento: ";
+	cout << "Elija un Departamento: ";
 	cin >> opDepartamento;
 
     // Imprimimos el string que se va a retornar por la función
@@ -261,7 +441,8 @@ int main()
 	"6.Psicologia","7.Derecho","8.Auditoria","9.Criminologia","10.Trabajo Social","11.Salir"};
 	
 	//se imprimen los encabezados
-	cout <<"Universidad Mariano Galvez de Guatemala, Campus Jutiapa";
+	cout << "Universidad Mariano Galvez de Guatemala, Campus Jutiapa";
+	cout << "\nY Conocereis La Verdad Y La Verdad Os Hara Libres\" Juan8:32";
 	cout << "\n\n\tElija la Carrera del Estudiante\n";
 	
 	for (int i = 0; i<=10 ; i++){
